5a1d31d3956eb15f296b92d5912feaf93f9a0f6b
Saksham Kohli <122266867+sakshamk007@users.noreply.github.com>
2024-05-12 12:35:14 +0530

    added mobile responsivity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <nav className='flex justify-around items-center bg-purple-700 text-black text-2xl h-12'>
-        <h1>ToDoList</h1>
-        <ul className='flex gap-2'>
+      <nav className='flex md:justify-around items-center bg-purple-700 text-black md:text-2xl h-12 text-xl justify-between'>
+        <h1 className='mx-2'>ToDoList</h1>
+        <ul className='flex md:gap-2'>
             <li className='list-none mx-2'>Home</li>
             <li className='list-none mx-2'>Tasks</li>
             <li className='list-none mx-2'>Finished Tasks</li>
